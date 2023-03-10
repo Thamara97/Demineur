90c61b2f299a196bef91748de3335a41c10d64e3
Thamara Renoir <tha.renoir@gmail.com>
2023-03-10 16:41:19 +0100

    Update shiny

--- demineurER/R/shiny.R
+++ demineurER/R/shiny.R
@@ -1,33 +1,60 @@
 library(shiny)
+library(shinyjs)
 
 ui <- fluidPage(
   titlePanel("Démineur"),
+
   sidebarLayout(
-    sidebarPanel(
-      sliderInput("n_mines", "Nombre de mines :", min = 10, max = 100, value = 30),
-      actionButton("reset", "Nouvelle partie")
+
+      sidebarPanel(
+
+          sliderInput("n_mines", "Nombre de mines :", min = 10, max = 100, value = 10),
+
+          sliderInput('ligne', "Nombre de lignes :", 5, min = 4, max = 30),
+
+          sliderInput('colonne', "Nombre de colonnes :", 5, min = 4, max = 30),
+
+          actionButton("reset", "Nouvelle partie"),
+
+          numericInput('case', "Case à creuser :", 1, min = 1, max = 30),
+
+          actionButton("go","Creuser", icon = icon("trowel"))
     ),
+
     mainPanel(
-      tableOutput("board")
+      tableOutput("board0"),
+      tableOutput("board1")
     )
-  )
+ )
 )
 
 server <- function(input, output, session) {
+
   n_mines <- reactive({
     input$n_mines
   })
-  
-  board <- reactive({
-    matrix(rep(" ", 100), ncol = 10, nrow = 10)
+
+  L <- reactive({input$ligne})
+  C <- reactive({input$colonne})
+
+  board <- eventReactive(input$reset, {
+    matrix(1:(L()*C()), nrow=L(), ncol=C())
   })
-  
-  output$board <- renderTable({
+
+  G <- eventReactive(input$reset, {grille(L(), C(), n_mines())})
+
+  observeEvent(input$reset, {hide("board1")})
+
+  output$board0 <- renderTable({
     board()
-  })
-  
-  observeEvent(input$reset, {
-    board() <- matrix(rep(" ", 100), ncol = 10, nrow = 10)
-  })
+  }, colnames = FALSE)
+
+  x <- eventReactive(input$go, {input$case})
+  observeEvent(input$go, {hide("board0")})
+
+  output$board1 <- renderTable({
+    creuser(board(), G(), x())
+  },colnames = FALSE)
 }
+
 shinyApp(ui, server)
