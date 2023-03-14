12f6f325a047a1d0c2d066d585f4b3c00f6689e6
Thamara Renoir <tha.renoir@gmail.com>
2023-03-14 17:59:03 +0100

    Début de code pour creuser les zones vides en une fois

--- demineurER/R/jeu.R
+++ demineurER/R/jeu.R
@@ -1,11 +1,33 @@
 creuser <- function(plateau, grille, case){
+
   L <- nrow(plateau)
   C <- nrow(plateau)
+
   if (grille[case] != "bombe") {
     plateau[case] <- paste(grille[case], "bombe", sep="")
+    if (grille[case] == "0") {plateau <-  astuce(plateau, grille, case)}
     return(plateau)
   }
+
   else {
     return(grille)
   }
+
+}
+
+astuce <- function(plateau, grille, case) {
+
+  L <- nrow(plateau)
+  C <- ncol(plateau)
+
+  if (case-1 > 0) {if (grille[case-1]=="0") {plateau[case-1] <- "0bombe"}}
+  if (case+1 < L*C+1) {if (grille[case+1]=="0") {plateau[case+1] <- "0bombe"}}
+  if (case-L > 0) {if (grille[case-L]=="0") {plateau[case-L] <- "0bombe"}}
+  if (case+L < L*C+1) {if (grille[case+L]=="0") {plateau[case+L] <- "0bombe"}}
+  if (case+L+1 < L*C+1) {if (grille[case+L+1]=="0") {plateau[case+L+1] <- "0bombe"}}
+  if (case+L-1 < L*C+1) {if (grille[case+L-1]=="0") {plateau[case+L-1] <- "0bombe"}}
+  if (case-L+1 > 0) {if (grille[case-L+1]=="0") {plateau[case-L+1] <- "0bombe"}}
+  if (case-L-1 > 0) {if (grille[case-L-1]=="0") {plateau[case-L-1] <- "0bombe"}}
+
+  return(plateau)
 }
