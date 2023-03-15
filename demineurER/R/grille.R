ccd336650c057ed3bdc01fd5d6a043be9f6bb7b2
Thamara Renoir <tha.renoir@gmail.com>
2023-03-15 17:08:24 +0100

    MAJ fonction grille

--- demineurER/R/grille.R
+++ demineurER/R/grille.R
@@ -12,32 +12,17 @@ grille <- function(L, C, nbr_bombe){
   }
 
   # positionnement des nombres dans l'intérieure de la grille
-  for (i in 2:(L-1)) {
-    for (j in 2:(C-1)) {
+  for (i in 1:(L)) {
+    l1 <- max(c(i-1,1))
+    l2 <- min(c(i+1, L))
+    for (j in 1:(C)) {
+      c1 <- max(c(j-1,1))
+      c2 <- min(c(j+1, C))
       if (g[i,j] !="bombe") {
-        g[i,j] <- sum(g[(i-1):(i+1),(j-1):(j+1)]=="bombe")
+        g[i,j] <- sum(g[l1:l2,c1:c2]=="bombe")
       }
     }
   }
 
-  # positionnement des nombres sur les bords horizontaux
-  for (j in 2:(C-1)) {
-    if (g[1,j] !="bombe") {g[1,j] <- sum(g[1:2,(j-1):(j+1)]=="bombe")}
-    if (g[L,j] !="bombe") {g[L,j] <- sum(g[(L-1):L,(j-1):(j+1)]=="bombe")}
-  }
-
-  # positionnement des nombres sur les bords verticaux
-  for (i in 2:(L-1)) {
-    if (g[i,1] !="bombe") {g[i,1] <- sum(g[(i-1):(i+1),1:2]=="bombe")}
-    if (g[i,C] !="bombe") {g[i,C] <- sum(g[(i-1):(i+1),(C-1):C]=="bombe")}
-  }
-
-  # positionnement des nombres aux coins
-  if (g[1,1] !="bombe") {g[1,1] <- sum(g[1:2,1:2]=="bombe")}
-  if (g[L,C] !="bombe") {g[L,C] <- sum(g[(L-1):L,(C-1):C]=="bombe")}
-  if (g[1,C] !="bombe") {g[1,C] <- sum(g[1:2,(C-1):C]=="bombe")}
-  if (g[L,1] !="bombe") {g[L,1] <- sum(g[(L-1):L,1:2]=="bombe")}
-
   return(g)
 }
-
