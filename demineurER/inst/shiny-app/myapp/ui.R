84fc26005dc95790a9b525a15333bc1d9647b773
Thamara Renoir <tha.renoir@gmail.com>
2023-03-19 22:48:48 +0100

    Syntaxe

--- demineurER/inst/shiny-app/myapp/ui.R
+++ demineurER/inst/shiny-app/myapp/ui.R
@@ -3,8 +3,10 @@ library(shinyalert)
 
 shinyUI(fluidPage(
 
-  tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge, .js-irs-0 .irs-bar {background: BLACK}")),
-  tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge, .js-irs-1 .irs-bar {background: BLACK}")),
+  tags$style(HTML(".js-irs-0 .irs-single, .js-irs-0 .irs-bar-edge,
+                  .js-irs-0 .irs-bar {background: BLACK}")),
+  tags$style(HTML(".js-irs-1 .irs-single, .js-irs-1 .irs-bar-edge,
+                  .js-irs-1 .irs-bar {background: BLACK}")),
 
     #____Couleurs de l'arrière plan et des boutons cliquable
   tags$head(
@@ -16,11 +18,11 @@ shinyUI(fluidPage(
                  #drap{color:green; background-color:   }
                   #go{color:red;background-color:white;}
                 ")
+    )
+  ),
 
-    )),
   #Titre du jeu
-
-  titlePanel("Démineur"),
+  h1("Démineur 💣"),
 
   sidebarLayout(
 
@@ -30,22 +32,21 @@ shinyUI(fluidPage(
 
       hr(),
 
-      sliderInput('colonne', "Nombre de colonnes :", 5, min = 4, max = 30),
-#__________Boutton pour jouer
-      actionButton("reset", "Nouvelle-partie"),
+      sliderInput("colonne", "Nombre de colonnes :", 5, min = 4, max = 30),
+#__________Bouton pour jouer
+      actionButton("reset", "Nouvelle partie"),
 
       hr(),
-#_________________Boutton pour creuser
-      numericInput('case', "Sélectionne une case :", 1, min = 1, max = 900),
-      actionButton("go","Creuser", icon = icon("trowel")),
+
+      numericInput("case", "Sélectionne une case :", 1, min = 1, max = 900),
+#_________________Bouton pour creuser
+      actionButton("go", "Creuser", icon = icon("trowel")),
 #___________________Drapeau
-      actionButton("drap","🚩"),
+      actionButton("drap", "🚩"),
 
     ),
 
     mainPanel(
-      useShinyalert(),
-      textOutput("rslt"),
       textOutput("bombe"),
       hr(),
       tableOutput("board")
