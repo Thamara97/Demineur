aee022c422188ba1f2585fa2a12f5cc67fe4f95d
Thamara Renoir <tha.renoir@gmail.com>
2023-03-19 22:47:45 +0100

    MAJ syntaxe + doc

--- demineurER/R/runapp.R
+++ demineurER/R/runapp.R
@@ -1,15 +1,16 @@
 #'@title Afficher le jeu
-#' Lancer l'application shiny "demineurER"
+#' @description Cette fonction permet de lancer l'application shiny du package
+#' "demineurER".
 #'
 #'
-#' @return Application shiny
+#' @return Application shiny "Démineur"
 #' @export
 #' @examples
 #' runapp()
 
-runapp <- function(){
+runapp <- function() {
 
-  appDir <- system.file("shiny-app","myapp",package = "demineurER")
+  appDir <- system.file("shiny-app", "myapp", package = "demineurER")
 
   if (appDir == "") {
     stop("Impossible d'insérer l'application.Essayez d'installer 'demineurER'.",
