e569c68f53d5dd347deeab5ad9a52e65a158b3b9
maryem-elyamani <meryamelyamani1999@gmail.com>
2023-03-19 14:37:50 +0100

    update

--- demineurER/R/runapp.R
+++ demineurER/R/runapp.R
@@ -1,3 +1,4 @@
+#'@title Afficher le jeu
 #' Lancer l'application shiny "demineurER"
 #'
 #'
@@ -7,8 +8,10 @@
 
 runapp <- function(){
   appDir <- system.file("shiny-app","myapp",package = "demineurER")
-  if (appDir == ""){
-    stop("Impossible d'insérer l'application.Essayez d'installer 'demineurER'.",call. = FALSE)
+  if (appDir == "")
+    {
+    stop("Impossible d'insérer l'application.Essayez d'installer 'demineurER'.",
+         call. = FALSE)
   }
   shiny::runApp(appDir, display.mode = "normal")
 }
