20dfc070f3d6b4516b55734d9530641c1c0331a0
maryem-elyamani <meryamelyamani1999@gmail.com>
2023-03-20 02:05:53 +0100

    UPDATE TIMER

--- demineurER/inst/shiny-app/myapp/server.R
+++ demineurER/inst/shiny-app/myapp/server.R
@@ -1,6 +1,39 @@
 library(shiny)
 
 shinyServer(function(input, output, session) {
+  # Initialize the timer, 10 seconds, not active.
+  timer <- reactiveVal()
+  active <- reactiveVal(FALSE)
+
+  # Output du temps restant.
+  output$timeleft <- renderText({
+    paste("Time left: ", seconds_to_period(timer()))
+  })
+
+  # observer that invalidates every second. If timer is active, decrease by one.
+  observe({
+    invalidateLater(10, session)
+    isolate({
+      if(active())
+      {
+        timer(timer()-1)
+        if(timer()<1)
+        {
+          active(FALSE)
+          showModal(modalDialog(
+            title = "Important message",
+            "Countdown completed!"
+          ))
+        }
+      }
+    })
+  })
+
+  # observers for actionbuttons
+  observeEvent(input$start, {active(TRUE)})
+  observeEvent(input$stop, {active(FALSE)})
+  observeEvent(input$reset, {timer(input$seconds)
+    })
 
 
 
@@ -86,37 +119,6 @@ shinyServer(function(input, output, session) {
     values1$c1 <- c()
     })
 
-  # Initialize the timer, 10 seconds, not active.
-  timer <- reactiveVal()
-  active <- reactiveVal(FALSE)
 
-  # Output du temps restant.
-  output$timeleft <- renderText({
-    paste("Time left: ", seconds_to_period(timer()))
-  })
-
-  # observer that invalidates every second. If timer is active, decrease by one.
-  observe({
-    invalidateLater(2000, session)
-    isolate({
-      if(active())
-      {
-        timer(timer()-1)
-        if(timer()<1)
-        {
-          active(FALSE)
-          showModal(modalDialog(
-            title = "Important message",
-            "Countdown completed!"
-          ))
-        }
-      }
-    })
-  })
-
-  # observers for actionbuttons
-  observeEvent(input$start, {active(TRUE)})
-  observeEvent(input$stop, {active(FALSE)})
-  observeEvent(input$reset, {timer(input$seconds)})
 
 })
